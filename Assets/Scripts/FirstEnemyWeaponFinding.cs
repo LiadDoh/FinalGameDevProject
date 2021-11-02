99c5fcb1beb6c9941d32b866e2e38c130a1ac3de
roeeavir <44678382+roeeavir@users.noreply.github.com>
2021-11-02 18:42:56 +0200

    First enemy picks up weapon

---
+++ Assets/Scripts/FirstEnemyWeaponFinding.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FirstEnemyWeaponFinding : MonoBehaviour
+{
+    //Transform that NPC has to follow
+    public Transform transformToFollow;
+    //NavMesh Agent variable
+    NavMeshAgent agent;
+    public GameObject enemyOne;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Follow the player
+        agent.destination = transformToFollow.position;
+        if (!agent.isStopped)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        if (Vector3.Distance(agent.destination, enemyOne.transform.position) < 0.1)
+        {
+            animator.SetBool("isMoving", false);
+        }
+    }
+}
--- Assets/Scripts/PickCoin.cs
+++ Assets/Scripts/PickCoin.cs
@@ -6,11 +6,21 @@ public class PickCoin : MonoBehaviour
 {
     public GameObject rifle1;
     public GameObject rifle2;
+    public GameObject rifle3;
+    //public GameObject rifle4;
 
     private void OnTriggerEnter(Collider other)
     {
         gameObject.SetActive(false);
-        rifle1.SetActive(true);
-        rifle2.SetActive(true);
+        Debug.Log(other.tag);
+        if (other.tag == "Player")
+        {
+            rifle1.SetActive(true);
+            rifle2.SetActive(true);
+        } else
+        {
+            rifle3.SetActive(true);
+        }
+
     }
 }
