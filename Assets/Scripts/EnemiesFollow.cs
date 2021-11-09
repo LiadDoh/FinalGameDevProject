562a3cd9d5a60ca2c4c0be49b617669e7b924105
LiadDoh <49018252+LiadDoh@users.noreply.github.com>
2021-11-09 17:37:37 +0200

    Change EnemiesFollow script

--- Assets/Scripts/EnemiesFollow.cs
+++ Assets/Scripts/EnemiesFollow.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+public enum EnemyState {
+    PATROL,
+    CHASE
+}
+
 public class EnemiesFollow : MonoBehaviour
 {
+    private EnemyState currentState;
     //Transform that NPC has to follow
     public Transform transformToFollow = null;
     //NavMesh Agent variable
@@ -17,12 +23,45 @@ public class EnemiesFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentState = EnemyState.PATROL;
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+
+        if(currentState == EnemyState.PATROL)
+        {
+            Patrol();
+        }
+        if(currentState == EnemyState.CHASE)
+        {
+            Chase();
+        }
+    }
+
+    
+    public void getClosestRiflePosition(List<GameObject> positions)
+    {
+        GameObject closestPosition = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach (GameObject go in positions)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < distance)
+            {
+                closestPosition = go;
+                distance = curDistance;
+            }
+        }
+        transformToFollow = closestPosition.transform;
+    }
+
+    void Patrol()
     {
         if (transformToFollow != null)
         {
@@ -36,12 +75,6 @@ public class EnemiesFollow : MonoBehaviour
             if (Vector3.Distance(agent.destination, thisEnemy.transform.position) <= agent.stoppingDistance)
             {
                 animator.SetBool("isMoving", false);
-                if (thisEnemy.tag == "FirstEnemy")
-                {
-                    thisEnemy.GetComponent<EnemiesFollow>().enabled = false;
-                    thisEnemy.GetComponent<FirstEnemyMotion>().enabled = true;
-                }
-
             }
         } else if (beganSearching)
         {
@@ -51,25 +84,8 @@ public class EnemiesFollow : MonoBehaviour
         }
     }
 
-    public void getClosestRiflePosition(List<GameObject> activePositions)
+    void Chase()
     {
-        int index = 0;
-        float closestDistance = Vector3.Distance(activePositions[index].transform.position, transform.position);
 
-        for (int i = 1; i < activePositions.Count; i++)
-        {
-            float tempDistance = Vector3.Distance(activePositions[i].transform.position, transform.position);
-            if (closestDistance > tempDistance)
-            {
-                closestDistance = tempDistance;
-                index = i;
-            }
-        }
-
-        transformToFollow = activePositions[index].transform;
-        Debug.Log(gameObject.name + " Begun heading to " + transformToFollow.name);
-        activePositions.RemoveAt(index);
-
-        remainingActivePositions = activePositions;
     }
 }
