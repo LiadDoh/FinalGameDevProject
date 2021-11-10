c66581427bb93d1090b86f09106a86be020b1ef5
roeeavir <44678382+roeeavir@users.noreply.github.com>
2021-11-10 14:17:50 +0200

    mami

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,12 +1,15 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
-    public float range = 100f;
+    public float range = 50f;
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+
+    public GameObject bullet;
     public float impactForce = 0.1f;
     public float fireRate = 15f;
 
@@ -25,19 +28,30 @@ public class Gun : MonoBehaviour
     {
         muzzleFlash.Play();
         RaycastHit hit;
-        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-        {
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+            {
 
-            Debug.Log(hit.transform.name);
-            Target target = hit.transform.GetComponent<Target>();
-            if (target != null)
-            {
-                target.TakeDamage(damage);
+                Debug.Log(hit.transform.name);
+                bullet.transform.position = hit.point;
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * impactForce);
+                }
             }
-            if (hit.rigidbody != null)
-            {
-                hit.rigidbody.AddForce(-hit.normal * impactForce);
-            }
-        }
+    }
+
+    private IEnumerator spawnBullet()
+    {
+        GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+
+        yield return new WaitForSeconds(2);
+
+        Destroy(newBullet);
     }
 }
